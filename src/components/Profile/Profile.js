e0501c882ad2ee2440820fd5ba24549887806287
Talha Ahmed <aiforeveryoneai@gmail.com>
2021-07-29 21:24:55 -0400

    Completed the profile component
    Timeline is remaining

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,15 +1,18 @@
-import { Typography } from "@material-ui/core";
 import React from "react";
+import { Typography } from "@material-ui/core";
+
+import "./Profile.css";
+import picture from "../../assets/images/picture.jpg";
 
 const Profile = () => {
   return (
     <div className="profile container_shadow">
       <div className="profile_name">
         <Typography className="name">Talha</Typography>
-        <Typography className="name">Talha</Typography>
+        <Typography className="title">Title</Typography>
       </div>
       <figure className="profile_image">
-        <img src="" />
+        <img src={picture} alt="Talha" />
       </figure>
       <div className="profile_information">
         Insert Timeline
